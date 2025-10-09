91f29cea88b348650f71d64ea27522833da4a332
Yanfei <yanfeiii30@gmail.com>
2025-10-09 20:07:38 +0800

    HEaADER

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
 
+const Header = () => {
+  return (
+    <div>
+      <h2>TaskFlow</h2>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/add">Add Task</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
