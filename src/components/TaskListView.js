3fc861e4ec9722625fd8c2399299c5ab589b79a9
k4rosh1 <oronanraymark76@gmail.com>
2025-10-09 21:36:26 +0800

    Final Update

--- src/components/TaskListView.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import TaskList from "./TaskList";
-
-const TaskListView = ({ tasks, deleteTask }) => {
-  return <TaskList tasks={tasks} deleteTask={deleteTask} />;
-};
-
-export default TaskListView;
